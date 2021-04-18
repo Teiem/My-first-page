f547ff0c62dd5af1dc6ba692d1065efaf1cb642c
tim.londschien <tim.londschien@study.hs-duesseldorf.de>
2021-04-18 06:42:08 +0200

    general improvements

--- SCRIPT.js
+++ SCRIPT.js
@@ -1,108 +1,79 @@
+const ulpai = document.getElementById("para-fazer");
+const input = document.getElementById('aitem');
+const pesquisar = document.getElementById('pesquisar');
+
 let meusItems = [];
+
 window.onload = () => {
-    if(!localStorage.getItem("tudoLista")) {
-        localStorage.setItem("tudoLista", JSON.stringify([]));
-    } else {
-        meusItems =JSON.parse(localStorage.getItem("tudoLista"));
-        meusItems.forEach(item => displayItems(item));
-    }
+    if (!localStorage.getItem("tudoLista")) return localStorage.setItem("tudoLista", JSON.stringify([]));
+
+    meusItems = JSON.parse(localStorage.getItem("tudoLista"));
+    meusItems.forEach(displayItems);
 }
+
+
 
 function atualizaRelogio() {
-    var momentoAtual = new Date();
+    const date = new Date();
+    document.getElementById("data").textContent = new Intl.DateTimeFormat("en-GB").format(date);
+    document.getElementById("hora").textContent = new Intl.DateTimeFormat("en-GB", { hour: "numeric", minute: "numeric" }).format(date);
 
-    var vhora = momentoAtual.getHours();
-    var vminuto = momentoAtual.getMinutes();
-    var vsegundo = momentoAtual.getSeconds();
-
-    var vdia = momentoAtual.getDate();
-    var vmes = momentoAtual.getMonth() + 1;
-    var vano = momentoAtual.getFullYear();
-
-    if (vdia < 10) { vdia = "0" + vdia; }
-    if (vmes < 10) { vmes = "0" + vmes; }
-    if (vhora < 10) { vhora = "0" + vhora; }
-    if (vminuto < 10) { vminuto = "0" + vminuto; }
-    
-
-    dataFormat = vdia + " / " + vmes + " / " + vano;
-    horaFormat = vhora + " : " + vminuto;
-
-    document.getElementById("data").textContent = dataFormat;
-    document.getElementById("hora").textContent = horaFormat;
-
-    setTimeout("atualizaRelogio()", 1000);
+    setTimeout(atualizaRelogio, 1000);
 };
-atualizaRelogio();
 
-function criarItem(conteudo){
-    if (conteudo.length > 0){
-        //pega o elemento pai
-        var ulpai = document.getElementById("para-fazer");
-        //cria um novo elemento com tag "li"
-        var novoel = document.createElement("li");
 
-        //adiciona o conteudo
-        novoel.textContent = conteudo;
+function criarItem(conteudo) {
+    if (!conteudo.length) return;
 
-        //adiciona o click pra remover
-        novoel.addEventListener("click", removersi);
+    const novoel = document.createElement("li");
 
-        //atualiza a lista pro localStorage
-        meusItems.push(novoel.textContent);
+    novoel.textContent = conteudo;
+    novoel.addEventListener("click", removersi);
+    novoel.classList.add("adicionando");
+    ulpai.appendChild(novoel);
 
-        //adiciona animaçao
-        novoel.classList.add("adicionando");
-        
-        //adiciona o "li" para o elemento pai "ul"
-        ulpai.appendChild(novoel);
-
-        //atualiza o localStorage
-        localStorage.setItem("tudoLista", JSON.stringify(meusItems));
-    }
+    meusItems.push(conteudo);
+    localStorage.setItem("tudoLista", JSON.stringify(meusItems));
 }
 
-var input = document.getElementById('aitem');
-input.addEventListener("keydown", (e) =>{
-    if(e.keyCode == 13){
-        criarItem(input.value);
-        input.value = "";
-    }
+input.addEventListener("keydown", ({ key }) => {
+    if (key !== "Enter") return;
+
+    criarItem(input.value);
+    input.value = "";
 })
 
-var itens = document.querySelectorAll("li");
-itens.forEach(item=>item.addEventListener("click", removersi));
 
-function removersi(event){
-    // var titulo = event.target.textContent
-    var ulpai = document.getElementById("para-fazer");
-    var el = event.target.textContent;
-    var elemento = event.target;
-    console.log(elemento);
-    elemento.classList.remove("adicionando");
-    elemento.classList.add("excluindo");
-    meusItems = meusItems.filter((item) => item !== el);
+function removersi({ target }) {
+    // TODO, bug if multiple have same text
+
+    target.classList.remove("adicionando");
+    target.classList.add("excluindo");
+    meusItems = meusItems.filter(item => item !== target.textContent);
     localStorage.setItem("tudoLista", JSON.stringify(meusItems));
 
-    setTimeout(() => {
-        elemento.remove();
-    }, 200);
-    
+    setTimeout(() => target.remove(), 200);
 }
 
-var pesquisar = document.getElementById('pesquisar');
-pesquisar.addEventListener("keydown", (e) =>{
-    if(e.keyCode == 13){
-        var termos = pesquisar.value;
-        window.open(`https://www.google.com/search?q=${termos}`);
-        pesquisar.value = "";
-    }
+pesquisar.addEventListener("keydown", ({ key }) => {
+    if (key !== "Enter") return;
+
+    const termos = pesquisar.value;
+    window.open(`https://www.google.com/search?q=${termos}`);
+    pesquisar.value = "";
+
 })
 
-function displayItems(conteudo){
-    let ulpai = document.getElementById("para-fazer");
-    var novoel = document.createElement("li");
+function displayItems(conteudo) {
+    const novoel = document.createElement("li");
     novoel.textContent = conteudo;
     novoel.addEventListener("click", removersi);
     ulpai.appendChild(novoel);
 }
+
+
+
+/* INIT */
+
+atualizaRelogio();
+document.querySelectorAll("li").forEach(item => item.addEventListener("click", removersi));
